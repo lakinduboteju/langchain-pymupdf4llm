b0e2ef025b85b9c1147a1303637a3bc76d5bbe8e
Lakindu Boteju <lakinduboteju@gmail.com>
2025-02-19 11:50:23 +0700

    Expose PyMuPDF4LLMLoader from module's __init__.py

--- langchain_pymupdf4llm/__init__.py
+++ langchain_pymupdf4llm/__init__.py
@@ -9,3 +9,5 @@ except metadata.PackageNotFoundError:
     # Case where package metadata is not available.
     __version__ = ""
 del metadata  # optional, avoids polluting the results of dir(__package__)
+
+from .pymupdf4llm_loader import PyMuPDF4LLMLoader
